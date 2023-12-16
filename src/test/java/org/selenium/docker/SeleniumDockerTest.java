5e31a12dc63b9b156cd93dc58a7291455b600394
Ravinder Gill <ravinder.gill@teamviewer.com>
2023-12-16 11:33:50 +0100

    docker ur commented

--- src/test/java/org/selenium/docker/SeleniumDockerTest.java
+++ src/test/java/org/selenium/docker/SeleniumDockerTest.java
@@ -1,6 +1,7 @@
 package org.selenium.docker;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
@@ -29,10 +30,13 @@ public class SeleniumDockerTest {
         System.out.println("=====================Test Class=======================");
 
 
-        URL url = new URL("http://localhost:4444");
+    /*    URL url = new URL("http://localhost:4444");
         DesiredCapabilities cap = new DesiredCapabilities();
         cap.setBrowserName("firefox");
         driver = new RemoteWebDriver(url, cap);
+     */
+        driver = new ChromeDriver();
+
         driver.get("https://alchemy.hguy.co/jobs/wp-admin");
         driver.findElement(By.id("user_login")).sendKeys(Username);
         driver.findElement(By.id("user_pass")).sendKeys(Password);
